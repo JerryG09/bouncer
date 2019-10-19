2c1b457d9acca83f8aaf74b075ac7682cf75616b
JerryG09 <phemmy09@gmail.com>
2019-10-19 22:47:02 +0100

    added Navbar

--- src/commons/header/Header.js
+++ src/commons/header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderTop from './HeaderTop';
 import Bouncer from './Bouncer';
+import Navbar from './Navbar';
 
 function Header() {
   return (
     <div>
       <HeaderTop />
       <Bouncer />
+      <Navbar />
     </div>
   );
 }
