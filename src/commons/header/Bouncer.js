b7f03cf7a9ca8fba6a0fa93a9cb02dfad67d9ddc
JerryG09 <phemmy09@gmail.com>
2019-10-19 22:32:23 +0100

    added Bouncer

--- src/commons/header/Bouncer.js
+++ src/commons/header/Bouncer.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Bouncer.css'
 
 function Bouncer() {
-  return <h4>hi</h4>;
+  return (
+    <div className="container">
+      <div className="">
+        <h4 className="logo">BOUNCER</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Bouncer;
