fa8e1707ca6ee22312b76123ce1342614a4d4096
JerryG09 <phemmy09@gmail.com>
2019-10-21 14:35:36 +0100

    added payment icons

--- src/commons/footer/FootoerBottom.js
+++ src/commons/footer/FootoerBottom.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import './FooterBottom.css'
 
 function FootoerBottom() {
   return (
     <div className="container">
-      <div className="row justify-content-between">
+      <div className="row justify-content-between pb-4">
         <div className="footer--bottom--left">
           <p> 2018 Ecommerce theme by www.bisenbaev.com</p>
         </div>
-        <div className="footer--bottom--right">
-          <div className="footer--western--union"></div>
-          <div className="footer--mastercard"></div>
-          <div className="footer--paypal"></div>
-          <div className="footer--visa"></div>
+        <div className="d-flex footer--bottom--right">
+          <div className="footer--western--union">
+            <img
+              src={require('../../assets/images/western-union.png')}
+              className="footer--payment--icons2"
+              alt="western union"
+            />
+          </div>
+          <div className="footer--mastercard pl-2">
+            <img
+              src={require('../../assets/images/mastercard.jpg')}
+              className="footer--payment--icons"
+              alt="mastercard"
+            />
+          </div>
+          <div className="footer--paypal pl-2">
+            <img
+              src={require('../../assets/images/paypal.jpg')}
+              alt="paypal"
+              className="footer--payment--icons2"
+            />
+          </div>
+          <div className="footer--visa pl-2">
+            <img
+              src={require('../../assets/images/visa.png')}
+              alt="visa"
+              className="footer--payment--icons"
+            />
+          </div>
         </div>
       </div>
     </div>
